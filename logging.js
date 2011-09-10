5ff3b10faef88f42a32cb8edb184fa53d96fcde0
Branko Vukelic <bg.branko@gmail.com>
2011-09-10 12:25:03 +0200

    Added request logger middleware

--- logging.js
+++ logging.js
@@ -8,10 +8,10 @@
 var c = require('colors');
 var util = require('util');
 
-var level = 'info';
+var level = 'debug';
 var LEVELS = {
-  info: 1,
-  debug: 2,
+  debug: 1,
+  info: 2,
   error: 3,
   critical: 4
 };
@@ -25,9 +25,23 @@ function getStamp() {
     '[', ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][date.getDay()], ' ',
     date.getFullYear(), '-', (date.getMonth() + 1), '-', date.getDate(),
     ' ', date.getHours(), ':', date.getMinutes(), ':', date.getSeconds(), '.',
-    date.getMilliseconds(), ']'
+    date.getMilliseconds(), ' GMT', date.getTimezoneOffset() / 60, ']'
   ].join('').grey;
 
+}
+
+function prettyPrintObj(o) {
+  if (!o || typeof o !== 'object' || !Object.keys(o).length) {
+    return '*'.grey + ' ' + 'n/a'.green + '\n';
+  }
+
+  var rows = [];
+
+  Object.keys(o).forEach(function(key) {
+    rows.push('*'.grey + ' ' + key.green + ': ' + o[key].toString());
+  });
+
+  return rows.join('\n') + '\n';
 }
 
 function log(msg, flag, minlvl, trace, block) {
@@ -47,6 +61,8 @@ function log(msg, flag, minlvl, trace, block) {
     console.trace();
   }
 }
+
+logging.pretty = prettyPrintObj;
 
 logging.setLevel = function(lvl) {
   if (Object.keys(LEVELS).indexOf(level) < 0) {
@@ -76,4 +92,40 @@ logging.inspect = function(obj, trace) {
   logging.debug(utils.inspect(obj, true, null), trace);
 };
 
+logging.requestLogger = function(req, res, next) {
+  var startTime = (new Date()).getTime();
+  var log = 'Request for '.green.bold + req.url.toString().yellow.bold + '\n\n';
 
+  res.on('finish', function() {
+    var endTime = (new Date()).getTime();
+    
+    log += 'Total request time: '.cyan.bold;
+    log += ((endTime - startTime) + 'ms\n').yellow.bold;
+
+    logging.dbg(log);
+  });
+  
+  log += 'Request details:\n'.cyan.bold;
+
+  log += '\n';
+
+  log += 'Request parameters:\n'.cyan.italic;
+  log += prettyPrintObj(req.params) + '\n';
+
+  log += 'URL parameters:\n'.cyan.italic;
+  log += prettyPrintObj(req.query) + '\n';
+
+  log += 'Request body:\n'.cyan.italic;
+  log += prettyPrintObj(req.body) + '\n';
+  log += '==/\n\n'.cyan.bold;
+
+  log += 'Memory usage:\n'.cyan.bold;
+  log += prettyPrintObj(process.memoryUsage());
+  log += '==/\n\n'.cyan.bold;
+
+  log += 'Request headers:\n'.cyan.bold;
+  log += prettyPrintObj(req.headers);
+  log += '==/\n\n'.cyan.bold;
+
+  next();
+};
