d7eb452cefeb979383540c5fc2fa0eb39962179f
Branko Vukelic <branko@brankovukelic.com>
2011-09-25 13:34:11 +0200

    Added pretty-print example

--- example.js
+++ example.js
@@ -5,3 +5,13 @@ logging.dbg('test debug');
 logging.err('test err');
 logging.bad('test bad');
 
+logging.dbg('Pretty-print test:\n' + logging.pretty({
+  a: 1,
+  b: 'foo',
+  c: { test: 'me' },
+  d: [1,2,3,4,5],
+  e: null,
+  f: undefined,
+  g: new Date()
+}));
+
